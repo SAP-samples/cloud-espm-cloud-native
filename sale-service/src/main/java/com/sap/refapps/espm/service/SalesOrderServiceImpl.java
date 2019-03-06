9595395bf2f0fa9022eb8025e7c5a5ca9367de3e
I311636 <vipin.vijaykumar@sap.com>
2019-03-06 11:53:06 +0530

    Destination Service Feature Added

--- sale-service/src/main/java/com/sap/refapps/espm/service/SalesOrderServiceImpl.java
+++ sale-service/src/main/java/com/sap/refapps/espm/service/SalesOrderServiceImpl.java
@@ -1,19 +1,30 @@
 package com.sap.refapps.espm.service;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.net.URI;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.Date;
+import java.util.HashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.cloudfoundry.com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.cloud.cloudfoundry.com.fasterxml.jackson.databind.DeserializationFeature;
+import org.springframework.cloud.cloudfoundry.com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.cloud.cloudfoundry.com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.core.env.Environment;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
@@ -38,11 +49,23 @@ public class SalesOrderServiceImpl implements SalesOrderService {
 	@Value("${tax.service}")
 	private String taxServiceEndPoint;
 
+	@Value("${tax.destinationName}")
+	private String taxDestination;
+
 	private Iterable<SalesOrder> salesOrder;
 
 	private QueueDispatcherService queueDispatcherService;
 
 	private String taxUri;
+
+    final ObjectMapper mapper = new ObjectMapper();
+
+    private HashMap<String,String> taxUrlCache = new HashMap<>(1);
+
+
+    final HttpHeaders headers = new HttpHeaders();
+
+    final static String DESTINATION_PATH = "/destination-configuration/v1/destinations/";
 	
 
 
@@ -61,6 +84,7 @@ public class SalesOrderServiceImpl implements SalesOrderService {
 		this.salesOrderRepository = salesOrderRepository;
 		this.queueDispatcherService = queueDispatcherService;
 		this.restTemplate = rest;
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 	}
 
 	/*
@@ -131,16 +155,39 @@ public class SalesOrderServiceImpl implements SalesOrderService {
 	 */
 	@HystrixCommand(fallbackMethod = "taxServiceFallback", raiseHystrixExceptions = HystrixException.RUNTIME_EXCEPTION, commandKey = "taxCommandKey", groupKey = "taxThreadPoolKey")
 	public Tax getTax(BigDecimal amount) {
-		Tax tax;
-		taxUri = Arrays.stream(environment.getActiveProfiles())
-				.anyMatch(env -> (env.equalsIgnoreCase("cloud"))) ? this.environment.getProperty("TAX_SERVICE")
-				: taxServiceEndPoint;
-		logger.info("Tax service endpoint is {}", taxUri);
-		logger.info("Tax service is called to calculate tax for amount : {}", amount);
-		URI uri = URI.create(taxUri + amount);
-		tax = this.restTemplate.getForObject(uri, Tax.class);
-		logger.info("Tax amount is : {}", tax.getTaxAmount());
-		return tax;
+        Tax tax;
+        taxUri = taxUrlCache.get("TAX_URI");
+        if (taxUri == "" || taxUri == null){
+            taxUri = getTaxUrlFromDestinationService();
+            taxUrlCache.put("TAX_URI",taxUri);
+        }
+
+
+        //If destination service not configured
+        if (taxUri == "") {
+            taxUri = Arrays.stream(environment.getActiveProfiles())
+                    .anyMatch(env -> (env.equalsIgnoreCase("cloud"))) ? this.environment.getProperty("TAX_SERVICE")
+                    : taxServiceEndPoint;
+            URI uri = URI.create(taxUri + amount);
+            tax = this.restTemplate.getForObject(uri, Tax.class);
+        }
+        else
+            try{
+                URI uri = URI.create(taxUri + amount);
+                tax = restTemplate.getForObject(uri, Tax.class);
+            }catch(HttpClientErrorException e){
+                logger.info("Retrying");
+                taxUri = getTaxUrlFromDestinationService();
+                taxUrlCache.put("TAX_URI",taxUri);
+                URI uri = URI.create(taxUri + amount);
+                tax = restTemplate.getForObject(uri, Tax.class);
+
+
+            }
+        logger.info("Tax service endpoint is {}", taxUri);
+        logger.info("Tax service is called to calculate tax for amount : {}", amount);
+        logger.info("Tax amount is : {}", tax.getTaxAmount());
+        return tax;
 	}
 
 	/*
@@ -158,5 +205,61 @@ public class SalesOrderServiceImpl implements SalesOrderService {
 
 		return tax;
 	}
+
+    private String getTaxUrlFromDestinationService(){
+        try {
+            final DestinationService destination = getDestinationServiceDetails();
+            final String accessToken = getOAuthToken(destination);
+            headers.set("Authorization","Bearer "+accessToken);
+            HttpEntity entity = new HttpEntity(headers);
+            final String taxUrl = destination.uri + DESTINATION_PATH + taxDestination ;
+            final ResponseEntity<String> response = restTemplate.exchange( taxUrl, HttpMethod.GET,entity,String.class  );
+            final JsonNode root = mapper.readTree(response.getBody());
+            final String texDestination = root.path("destinationConfiguration").path("URL").asText();
+            return texDestination;
+        }catch (IOException e){
+            logger.error("No proper destination Service available: {}", e.getMessage());
+
+        }
+        return "";
+    }
+
+
+    private String getOAuthToken(final DestinationService destination) throws IOException {
+        final String auth = destination.clientid+":"+destination.clientsecret;
+        final byte[] basicToken = Base64.getEncoder().encode(auth.getBytes());
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.set("Authorization","Basic "+new String(basicToken));
+        MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+        map.add("client_id", destination.clientid);
+        map.add("grant_type", "client_credentials");
+        final HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity( destination.url+"/oauth/token", request , String.class );
+        final String responseString = response.getBody();
+        JsonNode root = mapper.readTree(responseString);
+        final String accessToken = root.get("access_token").asText();
+        return accessToken;
+
+    }
+
+
+    private DestinationService getDestinationServiceDetails() throws IOException {
+        final String destinationService = System.getenv("VCAP_SERVICES");
+        final JsonNode root = mapper.readTree(destinationService);
+        final JsonNode destinations = root.get("destination").get(0).get("credentials");
+        final DestinationService destination = mapper.treeToValue(destinations,DestinationService.class);
+        return  destination;
+
+    }
+
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class DestinationService{
+
+    public String clientid;
+    public String clientsecret;
+    public String uri;
+    public String url;
 
 }
