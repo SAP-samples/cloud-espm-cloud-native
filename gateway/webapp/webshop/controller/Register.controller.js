7dfcc41316efe43974d5f803fd170a90d2341a51
Shankari G R <shankari.g.r@sap.com>
2022-05-18 15:17:34 +0530

    Update Register.controller.js

--- gateway/webapp/webshop/controller/Register.controller.js
+++ gateway/webapp/webshop/controller/Register.controller.js
@@ -504,7 +504,7 @@ sap.ui.define([
 					] };
 			// set explored app's demo model on this sample
 			var oModel = new sap.ui.model.json.JSONModel(oCountry);
-			oModel.setSizeLimit(1000);
+			oModel.setSizeLimit(250);
 			this.getView().setModel(oModel, "oModel");
 
 			var today = new Date();
